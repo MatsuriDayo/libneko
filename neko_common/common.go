f466d425f8817343431a8e0596bcb9d33abf1859
arm64v8a <48624112+arm64v8a@users.noreply.github.com>
2023-05-30 13:05:31 +0900

    update

--- neko_common/common.go
+++ neko_common/common.go
@@ -1,11 +1,13 @@
 package neko_common
 
 import (
+	"context"
+	"net"
 	"net/http"
 )
 
-var Version_v2ray string = "N/A"
-var Version_neko string = "N/A"
+var Version_v2ray = "N/A"
+var Version_neko = "N/A"
 
 var Debug bool
 
@@ -20,6 +22,25 @@ const (
 	RunMode_NekoBoxForAndroid
 )
 
-// proxy
+// proxy (if specifiedInstance==nil, access without proxy)
 
-var GetProxyHttpClient func() *http.Client
+var GetCurrentInstance func() interface{}
+
+var DialContext func(ctx context.Context, specifiedInstance interface{}, network, addr string) (net.Conn, error)
+
+// DialUDP core bug?
+var DialUDP func(ctx context.Context, specifiedInstance interface{}) (net.PacketConn, error)
+
+var CreateProxyHttpClient func(specifiedInstance interface{}) *http.Client
+
+// no proxy
+
+var NetDialer = &net.Dialer{}
+
+func DialContextSystem(ctx context.Context, network, addr string) (net.Conn, error) {
+	return NetDialer.DialContext(ctx, network, addr)
+}
+
+func DialUDPSystem(ctx context.Context) (net.PacketConn, error) {
+	return net.ListenUDP("udp", &net.UDPAddr{})
+}
